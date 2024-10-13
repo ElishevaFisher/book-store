c1a3420af701b938784d8d2fa0c3d62fc4066dfe
ElishevaFisher <7f2003@gmail.com>
2024-10-13 11:41:53 +0300

    delete book

--- view.js
+++ view.js
@@ -1,14 +1,14 @@
 const renderBook = (book) => {
   return `
-        <div class="book">
-            <h4 id="book-id">${book.id}</h4>
-            <button id="book-title" onclick='renderBookDetail(${JSON.stringify(
+        <div class="book" data-id="${book.id}">
+            <h4 class="book-id">${book.id}</h4>
+            <button class="book-title" onclick='renderBookDetail(${JSON.stringify(
               book
             )})'>${book.title}</button>
-            <h4 id="book-price">$${book.price}</h4>
-            <h4 id="book-read">read</h4>
-            <h4 id="book-update">update</h4>
-            <img src="garbige.jpg" alt="delete">
+            <h4 class="book-price">$${book.price}</h4>
+            <h4 class="book-read">read</h4>
+            <h4 class="book-update">update</h4>
+            <button class="deleteBtn"><img src="garbige.jpg" alt="delete"></button>
         </div>
     `;
 };
@@ -79,8 +79,8 @@ function loadFromLocalStorage() {
   try {
     if (storedData) {
       Gbooklist = JSON.parse(storedData);
-    } else {
-      saveToLocalstorage();
+      // } else {
+      //   saveToLocalstorage();
     }
     if (storedGid) {
       Gid = parseInt(storedGid, 10);
@@ -92,7 +92,7 @@ function loadFromLocalStorage() {
   renderBooks(Gbooklist);
 }
 
-document.getElementById("book-form").addEventListener("submit",(event)=>{
+document.getElementById("book-form").addEventListener("submit", (event) => {
   const title = document.getElementById("title").value;
   const price = parseFloat(document.getElementById("price").value);
   const img = document.getElementById("img").value;
@@ -113,6 +113,74 @@ document.getElementById("book-form").addEventListener("submit",(event)=>{
   } else {
     alert("Please fill in all fields correctly");
   }
-}) ;
- 
+});
+
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("deleteBtn")) {
+    const bookDiv = event.target.closest(".book");
+    const bookIdDelete = parseInt(bookDiv.dataset.id, 10);
+    Gbooklist = Gbooklist.filter((book) => book.id !== bookIdDelete);
+    
+    Gbooklist.forEach((book,index)=>{
+      book.id=index+1;
+    });
+    Gid=Gbooklist.length ? Gbooklist[Gbooklist.length-1].id+1 :1;
+
+    saveToLocalstorage();
+    renderBooks(Gbooklist);
+    updatePagBtn();
+  }
+});
+
+function resetData() {
+  Gbooklist = [
+    {
+      id: 1,
+      title: "The Covenant of Water",
+      price: 22,
+      img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT_jj5ajD4PQvEkjJ_px1jG0kQ5RygVFOZlvP75CMM5vIX6QDUadaaHQZhJ2_kepCCq4kSWzG-t6iqL3WmjzvmE1vGkjCmK2ZpnPq2WAaz3",
+    },
+    {
+      id: 2,
+      title: "The Seven Husbands of Evelyn Hugo",
+      price: 10,
+      img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTvfS4FNPtrDZRJjISp48_mpaU38JNs8AjAKySWjPui9YpIRqr2xCsKMYdZzwqOPx_UEzFTHdccN4gGTU50sXLz-lPeNjgDf7g2vmP-T2U",
+    },
+    {
+      id: 3,
+      title:
+        "How to Talk to Anyone: 92 Little Tricks for Big Success in Relationships",
+      price: 12,
+      img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQJ-YNXR1rTBZCOeQgt09tpW2eZdF5BXWQctFWxgnQMkQjTXPgwYcDcNMaOjLV6KTubB8aR9L_IpvLCe5RZv_jpnGwD3OZqe_tqLOx6d7IrVju-FUowvR19zA",
+    },
+    {
+      id: 4,
+      title: "Demon Copperhead",
+      price: 20,
+      img: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSDUlDNM-4OynzPJnnQ2GGtRFGYfCBGNi6OVoU2x0-NMDYC1i3iO_oF0Vut6OLIfREimnJ1fbnJpb268OkyXaEMpL8I30Ykew",
+    },
+    {
+      id: 5,
+      title: "Atria Books Ugly Love by Colleen Hoover",
+      price: 12,
+      img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcS12aFqHQzV6o7wk_5uHz7iJavSOyHVFZdJLkwFS3jKkQf_0R8xOu4Jn-Sqfxjy7xODZR62DrOOe8qLuq5iPrPNpJSzcgNWybjEemcwar4_iW5-KZcyw5AX",
+    },
+    {
+      id: 6,
+      title: "It Ends With Us",
+      price: 50,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLijs-FCt5jW8s5LCg_YMRoe7HZizicmJWUA&s",
+    },
+    {
+      id: 7,
+      title: "The Stalker by Alex Gray",
+      price: 30,
+      img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTXdKUybuuYAgblRfxVYuGWQ1NzMzGpzRGXQQw3flxR3s1XGVrI8ssMdXuGvNuBo018Tsr68VayaWADBkUJCeVkWgkNuzX2Ms9SjAlPKHPDfSEDW89QbvZc",
+    },
+  ];
+  Gid=7;
+  saveToLocalstorage();
+  renderBooks(Gbooklist);
+}
+
 
